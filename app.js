03e0e791e086b81711b38587027df185c42073b9
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-11-14 10:58:08 -0700

    devyn

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Meow");
+let cats = {
+  snibbley: {
+    name: "Mr. Snibbley",
+    mood: "Happy",
+    tolerance: Math.floor(Math.random() * 9) + 1,
+    img: "",
+    pets: 0
+  }
+};
