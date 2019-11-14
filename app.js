f2de7c3cdfbd035254aa107aadd1480689805e93
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-11-14 11:11:13 -0700

    ids done

--- app.js
+++ app.js
@@ -5,5 +5,26 @@ let cats = {
     tolerance: Math.floor(Math.random() * 9) + 1,
     img: "",
     pets: 0
+  },
+  wiskers: {
+    name: "Wiskers",
+    mood: "Happy",
+    tolerance: Math.floor(Math.random() * 9) + 1,
+    img: "",
+    pets: 0
+  },
+  batcat: {
+    name: "BatCat",
+    mood: "Happy",
+    tolerance: Math.floor(Math.random() * 9) + 1,
+    img: "",
+    pets: 0
   }
 };
+let activeCat = cats.snibbley;
+function pet(cat) {
+  activeCat.pets++;
+}
+function drawCat() {
+  document.querySelector("");
+}
